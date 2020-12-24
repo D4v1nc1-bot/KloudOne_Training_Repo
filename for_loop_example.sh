27d9a36f87a72475afa46ac3a3811aec29913bf5
Prashant Verma <vprashant49@gmail.com>
2020-12-24 13:44:53 +0530

    ADDITION

---
+++ for_loop_example.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for ((i=1; i<=10; i++))
+do
+
+echo "Dude, you’re my hero and s***"  #use echo -n if you want yor result in a single line
+done
+
+
---
+++ taking_input.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "[+] Who is your favourite character in Breaking Bad:"
+
+read character
+
+echo $character",not a bad choice but I like Walter White. XD"
